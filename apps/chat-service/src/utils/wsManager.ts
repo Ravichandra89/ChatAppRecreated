cb06bff9bc2d07aac274d923b4e99addeee25f64
Ravi chandra <githubravi01@gmail.com>
2025-03-11 23:23:35 +0530

    Logic to fetch the user serverId

---
+++ apps/chat-service/src/utils/wsManager.ts
@@ -0,0 +1,34 @@
+import { v4 as uuidv4 } from "uuid";
+import { sendMessage } from "../kafka/kafka.producer";
+import { addPendingRequest } from "../kafka/kafka.consumer";
+
+export const getUserWsServer = async (
+  userId: string
+): Promise<string | null> => {
+  return new Promise(async (resolve, reject) => {
+    const requestId = uuidv4();
+    addPendingRequest(requestId, resolve);
+
+    console.log(`📤 Requesting WebSocket server for user ${userId}`);
+
+    try {
+      await sendMessage("get-user-server-request", [
+        {
+          key: requestId,
+          value: JSON.stringify({
+            userId,
+            requestId,
+          }),
+        },
+      ]);
+
+      // Maintain the Timeout logic for fetching the I'd
+      setTimeout(() => {
+        reject(new Error("Timeout fetching WebSocket server"));
+      }, 5000);
+    } catch (error) {
+      console.error("❌ Error sending Kafka message:", error);
+      reject(error);
+    }
+  });
+};
