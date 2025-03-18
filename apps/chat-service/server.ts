50f547d8ae242ba800644aa34f7f605112fd4268
Ravi chandra <githubravi01@gmail.com>
2025-03-18 10:59:40 +0530

    ChatServer Main Server file

---
+++ apps/chat-service/server.ts
@@ -0,0 +1,34 @@
+import app from "./src/app";
+import dotenv from "dotenv";
+import prisma from "@repo/prisma";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 6002;
+
+const startServer = async () => {
+  try {
+    await prisma.$connect();
+    console.log("Connected to database successfully");
+
+    app.listen(PORT, () => {
+      console.log(`Chat Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+    process.exit(1);
+  }
+};
+
+// Executing the Server
+startServer();
+
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
+
+process.on("SIGTERM", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
